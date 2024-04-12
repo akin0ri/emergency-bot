07594766590c7761d7c92100b14aa1b6a848b445
akin0ri <akinori.u.2003.jp3@gmail.com>
2024-04-12 08:30:59 +0000

    fix STATIC_URL

--- work/emergency_bot/settings.py
+++ work/emergency_bot/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

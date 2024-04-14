ecb231fce45b34b3f40e3fe901922c86f06cd8f5
akin0ri <akinori.u.2003.jp3@gmail.com>
2024-04-14 03:27:07 +0000

    Add location message handling in linebot callback

--- app.py
+++ app.py
@@ -2,12 +2,18 @@ import os
 from os import environ
 from dotenv import load_dotenv
 from flask import Flask, request, abort
-from boto.s3.connection import S3Connection
 
 from linebot.v3 import WebhookHandler
 from linebot.v3.exceptions import InvalidSignatureError
 from linebot.v3.messaging import Configuration, ApiClient, MessagingApi, ReplyMessageRequest, TextMessage
-from linebot.v3.webhooks import MessageEvent, TextMessageContent
+from linebot.v3.webhooks import MessageEvent, TextMessageContent, LocationMessageContent
+from linebot.v3.messaging.models.location_action import LocationAction
+from linebot.v3.messaging import (
+    ReplyMessageRequest,
+    TextMessage,
+    QuickReply,
+    QuickReplyItem,    
+)
 
 # original file import area
 
@@ -65,14 +71,57 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
+    print('?'*10)
+    if event.message.text == "location":
+        with ApiClient(configuration) as api_client:
+            line_bot_api = MessagingApi(api_client)
+            items = [
+                QuickReplyItem(
+                    action=LocationAction(
+                        label="Location",
+                        text="位置情報を送信する"
+                    
+                    )
+                )
+            ]
+            quick_reply = QuickReply(items=items)
+
+            message=TextMessage(text='返信メッセージ', quickReply=quick_reply)
+
+            line_bot_api.reply_message_with_http_info(
+                        ReplyMessageRequest(
+                            reply_token=event.reply_token,
+                            messages=[message]
+                        )
+                    )
+                
+    else:
+        with ApiClient(configuration) as api_client:
+            line_bot_api = MessagingApi(api_client)
+            line_bot_api.reply_message_with_http_info(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    # messages=[TextMessage(text=f"your position is {event.message.latitude} {event.message.longitude}")]
+                    messages=[TextMessage(text=f"hoge")]  
+                )
+            )
+            
+# location message
+@handler.add(MessageEvent, message=LocationMessageContent)
+def handle_location(event):
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
-        line_bot_api.reply_message_with_http_info(
+        user_latitude = event.message.latitude
+        user_longitude = event.message.longitude
+
+        line_bot_api.reply_message(
             ReplyMessageRequest(
                 reply_token=event.reply_token,
-                messages=[TextMessage(text=event.message.text)]
-            )
-        )
+                messages=[TextMessage(text=f"緯度：{user_latitude}\n経度 : {user_longitude}")] 
+                )
+        )       
+
+
 if __name__ == '__main__':
     app.run(port=3131, debug=True)
     
